a302001ca91e7f70978058c5afbf8453eae9afc8
errezeeta <jramirezsamc@gmail.com>
2024-09-20 14:08:28 +0200

    Updates

---
+++ precache-manifest.6f09029f43e049cd719209738468ab9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c679eb89b682102ef773b8d20ffe056",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "9cb467d954d52cfb4fad",
+    "url": "/web/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/web/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/web/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "9cb467d954d52cfb4fad",
+    "url": "/web/static/js/main.e21e8cbf.chunk.js"
+  },
+  {
+    "revision": "fd6a28d169ff8f6a358d",
+    "url": "/web/static/js/runtime~main.dc1daebd.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/web/static/media/logo.06e73328.svg"
+  }
+]);
