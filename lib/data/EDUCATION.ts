f9bd52dac2d5f6be3fd46c8c49c0949e433fd63a
javi-RamirezM <javier.ramirez@truecold.io>
2025-11-12 01:10:01 +0100

    fx

--- lib/data/EDUCATION.ts
+++ lib/data/EDUCATION.ts
@@ -1,6 +1,7 @@
 export const EDUCATION = {
   institution: "Universitat Politècnica de Catalunya, Barcelona, Spain",
   degree: "Telematics Engineering",
+  description: "",
   since: "2017",
   until: "2022",
 };
